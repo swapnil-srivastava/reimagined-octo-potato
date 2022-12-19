57e28cd746fd0ba8411428ee108c7413a0570b6c
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2022-12-19 00:01:54 +0000

    fix: adding hello world service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloWorldController } from './controller/helloworld/helloworld.controller';
+import { HelloWorldService } from './services/helloWorld.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, HelloWorldController],
+  providers: [AppService, HelloWorldService],
 })
 export class AppModule {}
---
+++ src/controller/helloworld/helloworld.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { HelloWorldService } from '../../services/helloWorld.service';
+
+@Controller()
+export class HelloWorldController {
+  constructor(private readonly helloWorldService: HelloWorldService) {}
+
+  @Get('/helloworld')
+  getHelloWorld(): string {
+    return this.helloWorldService.getHelloWorld();
+  }
+}
