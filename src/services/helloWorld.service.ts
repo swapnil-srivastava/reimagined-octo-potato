64a5a2a09d39ee3196e8b49d3442f31d775ee984
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-20 19:29:57 +0000

    adding the change for checking the api

--- src/services/helloWorld.service.ts
+++ src/services/helloWorld.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HelloWorldService {
   getHelloWorld(): string {
-    return 'Hello Swapnil Srivastava from services!';
+    return 'Hello Swapnil Srivastava from Nest services!';
   }
 }
