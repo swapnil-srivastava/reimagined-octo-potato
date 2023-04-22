aab0bc49c4c44a351ebe16ac4a5276930a93f632
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-22 23:04:57 +0200

    adding the cors object with origin and methods

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://swapnilsrivastava.eu'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  });
   await app.listen(3000);
 }
 bootstrap();
