8737d71f00bd8fb2df12e5e2a8550a76504d792d
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-22 07:51:10 +0000

    confuring the cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: ['https://swapnilsrivastava.eu'],
+    methods: ['GET', 'POST'],
+    credentials: true,
   });
   await app.listen(3000);
 }
