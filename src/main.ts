bd387c5055d2aeca744bba9e1b4720ae3896c35f
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-23 00:55:47 +0200

    CHANGING IT TO FALSE

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: true,
+      origin: false,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       preflightContinue: false,
       optionsSuccessStatus: 204,
