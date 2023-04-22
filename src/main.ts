bd4348ef36c4f26514f8439e18e82823e21ce719
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-22 23:31:45 +0200

    adding the * for origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://swapnilsrivastava.eu'],
+    origin: ['https://swapnilsrivastava.eu/*'],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
   });
   await app.listen(3000);
