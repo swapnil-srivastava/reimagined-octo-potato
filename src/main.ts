cd33402f08091a2fda464d41ac82e1c7c67d27fe
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2023-04-23 00:02:14 +0200

    adding the cors object

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
