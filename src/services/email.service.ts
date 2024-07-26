69507e22d875236e79f7789a521f0181ef5c95e5
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 06:38:41 +0000

    add the headers in cors

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -8,28 +8,28 @@ export class EmailService {
   private client: postmark.Client;
 
   constructor(private readonly configService: ConfigService) {
-    this.emailKey = this.configService.get<string>('config.emailKey');
+    // this.emailKey = this.configService.get<string>('config.emailKey');
 
-    if (!this.emailKey) {
-      throw new Error('Postmark API key is not defined');
-    }
+    // if (!this.emailKey) {
+    //   throw new Error('Postmark API key is not defined');
+    // }
 
-    // Send an email:
-    this.client = new postmark.Client(this.emailKey);
+    // // Send an email:
+    // this.client = new postmark.Client(this.emailKey);
 
     console.log('Postmark client initialized');
   }
 
   async sendNestEmailService(to: string, subject: string, htmlBody: string): Promise<void> {
     try {
-      await this.client.sendEmail({
-        From: 'hello@swapnilsrivastava.eu',
-        To: to,
-        Subject: subject,
-        HtmlBody: htmlBody,
-        TextBody: 'Hello from Postmark!',
-        MessageStream: 'outbound',
-      });
+      // await this.client.sendEmail({
+      //   From: 'hello@swapnilsrivastava.eu',
+      //   To: to,
+      //   Subject: subject,
+      //   HtmlBody: htmlBody,
+      //   TextBody: 'Hello from Postmark!',
+      //   MessageStream: 'outbound',
+      // });
       console.log('Email sent successfully');
     } catch (error) {
       console.error('Error sending email:', error);
