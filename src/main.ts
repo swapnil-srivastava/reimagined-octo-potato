620edc6e03331ec9e4d6cc1b559e14632789eef7
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 08:18:46 +0000

    adding preFlightContinue

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    preflightContinue: false,
   });
   await app.listen(3000);
 }
