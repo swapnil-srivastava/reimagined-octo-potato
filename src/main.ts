6aac8cb4a10a68430ac2bc50be574bc20b568d8a
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 06:56:19 +0000

    removing allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
     origin: ['https://swapnilsrivastava.eu'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
-    allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(3000);
 }
