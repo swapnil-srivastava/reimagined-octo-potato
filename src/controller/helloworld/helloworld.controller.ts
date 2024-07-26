ef1172fbbd168683df8dd4904ed6dc585f40f408
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 07:30:39 +0000

    adding email service to hello world controller

--- src/controller/helloworld/helloworld.controller.ts
+++ src/controller/helloworld/helloworld.controller.ts
@@ -1,16 +1,34 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Options, Post } from '@nestjs/common';
 import { HelloWorldService } from '../../services/helloWorld.service';
+import { EmailService } from '../../services/email.service';
 
 @Controller('helloworld')
 export class HelloWorldController {
   private readonly logger = new Logger(HelloWorldController.name);
 
-  constructor(private readonly helloWorldService: HelloWorldService) {}
+  constructor(private readonly helloWorldService: HelloWorldService, private readonly emailService: EmailService) {}
 
   @Get()
   getHelloWorld(): string {
     this.logger.log('Received GET request for /helloworld endpoint');
     return this.helloWorldService.getHelloWorld();
+  }
+
+  @Post('/sendnestemail')
+  sendNestEmail(
+    @Body('to') to: string,
+    @Body('subject') subject: string,
+    @Body('htmlBody') htmlBody: string,
+  ) {
+    this.logger.log('Received POST request to send email');
+    console.log("sendnestemail", to, subject, htmlBody)
+    this.emailService.sendNestEmailService(to, subject, htmlBody);
+  }
+
+  @Options('sendnestemail')
+  options() {
+    this.logger.log('Received OPTIONS request');
+    return;
   }
 
 }
