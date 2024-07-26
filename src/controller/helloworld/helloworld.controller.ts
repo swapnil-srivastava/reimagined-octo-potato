9a6ccc49f0b90c7a1d77e97dbc315ee9648e9898
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 07:14:38 +0000

    adding the send email in hello world controller

--- src/controller/helloworld/helloworld.controller.ts
+++ src/controller/helloworld/helloworld.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger } from '@nestjs/common';
 import { HelloWorldService } from '../../services/helloWorld.service';
+import { EmailService } from '../../services/email.service';
 
-@Controller()
+@Controller('helloworld')
 export class HelloWorldController {
-  constructor(private readonly helloWorldService: HelloWorldService) {}
+  private readonly logger = new Logger(HelloWorldController.name);
+
+  constructor(private readonly helloWorldService: HelloWorldService, private readonly emailService: EmailService) {}
 
   @Get('/helloworld')
   getHelloWorld(): string {
     return this.helloWorldService.getHelloWorld();
+  }
+
+  @Post('/sendnestemail-hello')
+  sendNestEmail(
+    @Body('to') to: string,
+    @Body('subject') subject: string,
+    @Body('htmlBody') htmlBody: string,
+  ) {
+    this.logger.log('Received POST request to send email');
+    console.log("sendnestemail", to, subject, htmlBody)
+    this.emailService.sendNestEmailService(to, subject, htmlBody);
   }
 
   
