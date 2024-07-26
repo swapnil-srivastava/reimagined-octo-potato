29653c487fe6a97e9d00f83654656809a6ee52ef
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 05:31:11 +0000

    fix: adding the cors package and using bootstrap page

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: "https://swapnilsrivastava.eu",
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 204,
-    },
-  });
+  const app = await NestFactory.create(AppModule);
+  app.use(cors({
+    origin: 'https://swapnilsrivastava.eu',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
