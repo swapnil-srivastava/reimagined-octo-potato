cace6d1932602ecdc76162b95b938f91d4958fe2
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-07-26 07:54:00 +0000

    un commenting email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,39 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-// import * as postmark from 'postmark';
+import * as postmark from 'postmark';
 
 @Injectable()
 export class EmailService {
-  // private readonly emailKey: string;
-  // private client: postmark.Client;
+  private readonly emailKey: string;
+  private client: postmark.Client;
 
   constructor(private readonly configService: ConfigService) {
-    // this.emailKey = this.configService.get<string>('config.emailKey');
+    this.emailKey = this.configService.get<string>('config.emailKey');
 
-    // if (!this.emailKey) {
-    //   throw new Error('Postmark API key is not defined');
-    // }
+    if (!this.emailKey) {
+      throw new Error('Postmark API key is not defined');
+    }
 
-    // // Send an email:
-    // this.client = new postmark.Client(this.emailKey);
+    // Send an email:
+    this.client = new postmark.Client(this.emailKey);
 
     console.log('Postmark client initialized');
   }
 
-   sendNestEmailService(to: string, subject: string, htmlBody: string) {
+   async sendNestEmailService(to: string, subject: string, htmlBody: string) {
     try {
-      // await this.client.sendEmail({
-      //   From: 'hello@swapnilsrivastava.eu',
-      //   To: to,
-      //   Subject: subject,
-      //   HtmlBody: htmlBody,
-      //   TextBody: 'Hello from Postmark!',
-      //   MessageStream: 'outbound',
-      // });
-      console.log('Email sent successfully');
-      return {
-        name: 'swapnil'
-      }
+      await this.client.sendEmail({
+        From: 'hello@swapnilsrivastava.eu',
+        To: to,
+        Subject: subject,
+        HtmlBody: htmlBody,
+        TextBody: 'Hello from Postmark!',
+        MessageStream: 'outbound',
+      });
       console.log('Email sent successfully');
     } catch (error) {
       console.error('Error sending email:', error);
